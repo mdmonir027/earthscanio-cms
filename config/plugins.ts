fc22ff7a06dcf710299e080d23c28ceda3504e63
Md Monirul Islam <mmislam027@gmail.com>
2023-05-31 11:10:33 +0600

    remove thirdparty tinymce plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,24 +3,4 @@ module.exports = () => ({
     enabled: true,
     resolve: "./src/plugins/wysiwyg",
   },
-  tinymce: {
-    enabled: false,
-    config: {
-      editor: {
-        outputFormat: "html",
-        editorConfig: {
-          menubar: true,
-          plugins:
-            "advlist autolink lists link image charmap preview anchor \
-                    searchreplace visualblocks code fullscreen table emoticons nonbreaking \
-                    insertdatetime table code help wordcount",
-          toolbar:
-            "undo redo | styles | bold italic forecolor backcolor | \
-                    alignleft aligncenter alignright alignjustify | \
-                    table emoticons visualblocks code|\
-                    nonbreaking bullist numlist outdent indent | removeformat",
-        },
-      },
-    },
-  },
 });
